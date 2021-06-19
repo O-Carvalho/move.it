2794f6202d219759d216a370cbfb8701c0920831
Matheus Carvalho <matheus.dev23@gmail.com>
2021-06-19 15:29:52 -0300

    primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next';
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import styles from '../styles/pages/Home.module.css';
+import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
+
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move it</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+      </Head>
+   <ExperienceBar />
+
+
+   <CountdownProvider>
+        <section>
+        <div>     
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+
+        </div>
+        <div>
+          <ChallengeBox />
+
+        </div>
+    </section>
+   </CountdownProvider>
+  </div>
+  </ChallengesProvider>
+  )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+  
+  return {
+    props: {
+      level: Number(level),
+      currentExperience:Number(currentExperience),
+      challengesCompleted:Number(challengesCompleted),
+    }
+  }
+}
+
